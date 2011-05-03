5015bdf6904d52c01aa4ce2a01295d42b7c74780
Rafael Benevides <rafabene@gmail.com>
2011-05-03 18:20:28 -0300

    Layout adjustments

--- JavaSource/demo/MyFirstCDIBean.java
+++ JavaSource/demo/MyFirstCDIBean.java
@@ -5,7 +5,7 @@ import javax.inject.Named;
 
 @Named
 @Stateless
-public class MyFirstCDIBean {
+public class MyFirstCDIBean{
 
 	public String getHelloWorldMessage(){
 		return "Hello World from a CDI Bean";
