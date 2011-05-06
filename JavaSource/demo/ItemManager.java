8c862c951d71169f7c7646c0211824ef4b2c2cdd
Rafael Benevides <rafabene@gmail.com>
2011-05-06 17:36:32 -0300

    Parameter Injection Implementation done

--- JavaSource/demo/ItemManager.java
+++ JavaSource/demo/ItemManager.java
@@ -10,13 +10,18 @@ import javax.inject.Inject;
 
 import demo.domain.Item;
 import demo.domain.ItemRepository;
+import demo.infrastructure.qualifiers.Example;
 
 @ManagedBean
 @RequestScoped
 public class ItemManager {
 	
-	@Inject
 	private ItemRepository itemRepository;
+	
+	@Inject
+	public void setItemRepository(@Example ItemRepository itemRepository) {
+		this.itemRepository = itemRepository;
+	}
 	
 	public void execute(){
 		FacesContext fc = FacesContext.getCurrentInstance();
