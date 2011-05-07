b12b5e066137ab200cbf3ae381f1a53a99e267e4
Rafael Benevides <rafabene@gmail.com>
2011-05-06 21:53:43 -0300

    Removed Libs, Changed javax.faces to javax.enterprise.context, Implementation Done

--- JavaSource/demo/ItemManager.java
+++ JavaSource/demo/ItemManager.java
@@ -2,11 +2,11 @@ package demo;
 
 import java.util.List;
 
+import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import demo.domain.Item;
 import demo.domain.ItemRepository;
@@ -14,7 +14,7 @@ import demo.domain.OverSpeedHandlerService;
 import demo.domain.SpeedVerifierService;
 import demo.infrastructure.qualifiers.Example;
 
-@ManagedBean
+@Named("itemManager")
 @RequestScoped
 public class ItemManager {
 	
--- JavaSource/demo/infrastructure/JMSOverSpeedHandlerService.java
+++ JavaSource/demo/infrastructure/JMSOverSpeedHandlerService.java
@@ -2,17 +2,13 @@ package demo.infrastructure;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.inject.Inject;
 
 import demo.domain.Item;
-import demo.domain.ItemRepository;
 import demo.domain.OverSpeedHandlerService;
 
 public class JMSOverSpeedHandlerService implements OverSpeedHandlerService {
 	
-	@Inject
-	private ItemRepository itemRepository;
-	
+
 	@PostConstruct
 	public void openJMSConnection(){
 		System.out.println("Connecting to JMS");
@@ -20,7 +16,6 @@ public class JMSOverSpeedHandlerService implements OverSpeedHandlerService {
 
 	@Override
 	public void handle(Item item) {
-		System.out.println(itemRepository.getClass());
 		System.out.println("Posting " + item + " to Queue");
 
 	}
